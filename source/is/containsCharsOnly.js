36148341c6e16a9c9a8ed69b33baef467a5cf936
LazyGyu <lazygyu@gmail.com>
2014-02-26 18:27:22 +0900

    Update containsCharsOnly.js
    빈 문자열이 true 인게 맞느냐 아니냐의 경우에는 논쟁이 있을 수 있으므로 마지막 옵션으로 추가했어요

--- source/is/containsCharsOnly.js
+++ source/is/containsCharsOnly.js
@@ -2,21 +2,16 @@
  * 입력된 값이 정해진 문자열로만 이루어졌는지 확인
  * @param target   확인할 문자열
  * @param search   제한할 문자열들
+ * @param allowEmptyString true 일 경우 빈 문자열도 맞는걸로
  * @returns {Boolean}
  */
-clean.is.containsCharsOnly = function(target, search) {
+clean.is.containsCharsOnly = function(target, search, allowEmptyString) {
 	//REQUIRED: target
 	//REQUIRED: search
+	//OPTIONAL: allowEmptyString
 
 	var
-	// index~~~!!
-	i;
-
-	for ( i = 0; i < target.length; i += 1) {
-		if (search.indexOf(target.charAt(i)) === -1) {
-			return false;
-		}
-	}
-
-	return true;
+	// 정규표현식을 사용합시다!
+	r = new RegExp("^[" + search + "]"+(allowEmptyString?"*":"+")+"$");
+	return r.test(target);
 };
