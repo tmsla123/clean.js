968f3471847b97cd1abd3f91a5504231b3f8501b
Hanul <mr@hanul.co>
2014-02-26 23:49:46 +0900

    no message

--- source/object/each.js
+++ source/object/each.js
@@ -4,17 +4,17 @@ clean.object.each = function(object, callback) {
 	//REQUIRED: callback
 
 	var
-	// 프로퍼티의 이름에요.
-	name;
+	// 프로퍼티의 키에요.
+	key;
 
 	// 모든 프로퍼티를 둘러봅니다.
-	for (name in object) {
+	for (key in object) {
 
 		// 안전하게 객체에 잘 있나 확인하고,
-		if (object.hasOwnProperty(name) === true) {
+		if (object.hasOwnProperty(key) === true) {
 
-			// 프로퍼티를 callback으로 쏴줘요!
-			callback(object[name]);
+			// 프로퍼티의 값과 키를 callback으로 쏴줘요!
+			callback(object[key], key);
 		}
 	}
 };
