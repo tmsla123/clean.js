03e30860c66356d31b1dba57eec21a9b6175d1b8
Kyuhyen Hwang <lespinsideg@gmail.com>
2014-02-26 17:28:47 +0900

    Update string.js
    주석 오타수정
    object > Object

--- source/is/string.js
+++ source/is/string.js
@@ -5,7 +5,7 @@ clean.is.string = function(target) {
 	// 일단 퍼왔는데... 더 좋은코드 있음 써주세요.
 	// return toString.call(target) == '[object String]';
 
-        // IE에서 toString에 접근하기 위해서는 object.prototype.toString으로 접근해야합니다.
+        // IE에서 toString에 접근하기 위해서는 Object.prototype.toString으로 접근해야합니다.
         // 브라우져에 상관없이 쓸 수 있는 typeof을 쓰는 건 어떨가요?
         return typeof target === 'string';
 };
