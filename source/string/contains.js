433903979ba2aaddc929acf74e902ed313643732
Devyrlee <devyrlee@naver.com>
2014-02-26 15:07:35 +0900

    String 유틸, Contains유틸 추가

--- source/string/contains.js
+++ source/string/contains.js
@@ -7,3 +7,46 @@ clean.string.contains = function(target, search) {
 	return target.indexOf(search) >= 0;
 };
 
+//전화번호에 하이픈을 넣어서 반환한다
+clean.String.hyphenOnPhoneNum = function(target) {
+	
+	
+	var RegNotNum = /[^0-9]/g;
+	var RegPhonNum = "";
+	var DataForm = "";
+
+	if (str == "" || str == null)
+		return "";
+
+	str = str.replace(RegNotNum, '');
+
+	if (str.length < 4)
+		return str;
+
+	if (str.length > 3 && str.length < 7) {
+		DataForm = "$1-$2";
+		RegPhonNum = /([0-9]{3})([0-9]+)/;
+	} else if (str.length == 7) {
+		DataForm = "$1-$2";
+		RegPhonNum = /([0-9]{3})([0-9]{4})/;
+	} else if (str.length == 8) {
+		DataForm = "$1-$2-$3";
+		RegPhonNum = /([0-9]{4})([0-9]{2})([0-9]+)/;
+	} else if (str.length == 9) {
+		DataForm = "$1-$2-$3";
+		RegPhonNum = /([0-9]{2})([0-9]{3})([0-9]+)/;
+	} else if (str.length == 10) {
+		DataForm = "$1-$2-$3";
+		RegPhonNum = /([0-9]{3})([0-9]{3})([0-9]+)/;
+	} else {
+		DataForm = "$1-$2-$3";
+		RegPhonNum = /([0-9]{3})([0-9]{4})([0-9]+)/;
+	}
+
+	while (RegPhonNum.test(str)) {
+		str = str.replace(RegPhonNum, DataForm);
+	}
+	
+	return str;
+	
+}
