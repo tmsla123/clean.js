0a666937afeab3a79a2c57d806c11db58d193824
Hanul <mr@hanul.co>
2014-02-27 18:05:45 +0900

    filter와 find 추가~!

--- clean.js
+++ clean.js
@@ -52,6 +52,57 @@ clean.array.each = function(array, callback) {
 	}
 };
 
+// 배열에서 값들 찾아갖고 배열로 반환!
+clean.array.filter = function(array, check) {
+	//REQUIRED: array
+	//REQUIRED: check
+
+	var
+	// 찾은 값들
+	finds = [];
+
+	// 배열의 값을 하나씩 보면서,
+	clean.array.each(array, function(value) {
+
+		// 검사가 일치하면!
+		if (check(value) === true) {
+
+			// 값을 삽입!
+			finds.push(value);
+		}
+	});
+
+	// 최종적으로 찾은 값들 반환
+	return finds;
+};
+
+// 배열에서 값 찾기
+clean.array.find = function(array, check) {
+	//REQUIRED: array
+	//REQUIRED: check
+
+	var
+	// 찾은 값
+	find;
+
+	// 배열의 값을 하나씩 보면서,
+	clean.array.each(array, function(value) {
+
+		// 검사가 일치하면!
+		if (check(value) === true) {
+
+			// 값을 삽입!
+			find = value;
+			
+			// each 종료!
+			return;
+		}
+	});
+
+	// 최종적으로 찾은 값 반환
+	return find;
+};
+
 // 배열에서 최대 값 찾기
 clean.array.max = function(array) {
 	//REQUIRED: array
