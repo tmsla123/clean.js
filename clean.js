db6a2439118bc684de824fc9d06756306b66a665
Hanul <mr@hanul.co>
2014-03-01 20:06:00 +0900

    no message

--- clean.js
+++ clean.js
@@ -481,16 +481,19 @@ clean.dom.find = function(selectors) {
 };
 
 // 문서 높이 구하기인데... 마땅히 어디다가 둘때가;;;
-clean.dom.getDocHeight = function() {
-	//REQUIRED: 현재 문서의 높히를 구해요!
+//COMMENT: 여기 두심 됩니당!! ㅋㅋ 이름은 좀 변경 했어용!!
+clean.dom.docHeight = function() {
 
 	var d = document;
+	
+	// 현재 문서의 높히를 구해요!
 	return Math.max(
 		Math.max(d.body.scrollHeight, d.documentElement.scrollHeight),
 		Math.max(d.body.offsetHeight, d.documentElement.offsetHeight),
 		Math.max(d.body.clientHeight, d.documentElement.clientHeight)
 	);
 };
+
 // 몇초를 기다릴까나?
 clean.helper.delay = function(seconds, func) {
 
--- source/dom/docHeight.js
+++ source/dom/docHeight.js
@@ -1,8 +1,10 @@
 // 문서 높이 구하기인데... 마땅히 어디다가 둘때가;;;
-clean.dom.getDocHeight = function() {
-	//REQUIRED: 현재 문서의 높히를 구해요!
+//COMMENT: 여기 두심 됩니당!! ㅋㅋ 이름은 좀 변경 했어용!!
+clean.dom.docHeight = function() {
 
 	var d = document;
+	
+	// 현재 문서의 높히를 구해요!
 	return Math.max(
 		Math.max(d.body.scrollHeight, d.documentElement.scrollHeight),
 		Math.max(d.body.offsetHeight, d.documentElement.offsetHeight),
