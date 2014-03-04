bc279577005735650f953c5aeaf8a29af1004499
dkfdnd <dkfdnd@naver.com>
2014-03-04 23:44:57 +0900

    style 커밋

--- source/dom/style.js
+++ source/dom/style.js
@@ -6,14 +6,23 @@
 
 clean.dom.style = function(el,data){
 	
+	//datd가 object면 	
 	if(clean.is.object(data) === true) {
 		
+		//styleSet인 data에서 한개씩 뽑아내서 stlyeOne에 대입하여
+		for( var styleOne in data){
+			
+			///el의 styleOne에 매칭되는 styleProperty에 넘어온 data의 styleOne에 해당하는 value를 대입힌다.
+			el.style[styleOne]=data[styleOne];
+		}
 	}
-	
-	
+	//datd가 string이면
 	else if(clean.is.string(data) === true) {
 		
-		return el.getAttribute(data);
+		//el의 style이름이 data와 매칭 되는것을 반환 
+		//ex. el의 style 중  display = 'inline'이 있을때 
+		//data가 'display'면 inline반환
+		return getComputedStyle(el,null).getPropertyValue(data);
 	}
 
 };
