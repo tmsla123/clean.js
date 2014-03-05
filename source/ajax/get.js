878f33792801874dd97bc79f696fe89933705a6f
Hanul <mr@hanul.co>
2014-03-06 00:34:57 +0900

    put, del 추가

--- source/ajax/get.js
+++ source/ajax/get.js
@@ -3,37 +3,5 @@ clean.ajax.get = function(url, callback) {
 	//REQUIRED: url
 	//REQUIRED: callback
 
-	var
-	// http request
-	httpRequest;
-
-	if (window.XMLHttpRequest) {// Mozilla, Safari, ...
-		httpRequest = new XMLHttpRequest();
-	} else if (window.ActiveXObject) {// IE
-		try {
-			httpRequest = new ActiveXObject('Msxml2.XMLHTTP');
-		} catch (e) {
-			try {
-				httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
-			} catch (e) {
-			}
-		}
-	}
-
-	if (!httpRequest) {
-		alert('Giving up :( Cannot create an XMLHTTP instance');
-		return false;
-	}
-	httpRequest.onreadystatechange = function() {
-		if (httpRequest.readyState === 4) {
-			if (httpRequest.status === 200) {
-				callback(httpRequest.responseText);
-			} else {
-				alert('There was a problem with the request.');
-			}
-		}
-	};
-	httpRequest.open('GET', url);
-	httpRequest.send();
-
+	clean.ajax.call(url, 'GET', callback);
 };
